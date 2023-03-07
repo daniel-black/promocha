42cca536cb1e08379b1bad7ea597e21a573aa48d
daniel-black <drblack651@gmail.com>
2023-03-07 08:15:26 -0700

    work on landing page

--- src/app/(auth)/form.tsx
+++ src/app/(auth)/form.tsx
@@ -49,7 +49,7 @@ export default function Form({ type }: FormProps) {
             {...register('email')}
             type="email"
             placeholder="tim@apple.com"
-            className="w-full rounded p-3 outline-none bg-slate-200 hover:bg-slate-100 focus:bg-slate-50 focus:ring focus:ring-slate-400 focus:ring-inset focus:ring-offset-1 text-slate-900 text-lg transition-all duration-75"
+            className="w-full rounded p-3 outline-none bg-slate-200 shadow-sm hover:bg-slate-100 focus:bg-slate-50 focus:ring focus:ring-slate-400 focus:ring-inset focus:ring-offset-1 text-slate-900 text-lg transition-all duration-75"
             disabled={isSubmitting}
             required
           />
@@ -60,7 +60,7 @@ export default function Form({ type }: FormProps) {
           <input
             {...register('password')}
             type="password"
-            className="w-full rounded p-3 outline-none bg-slate-200 hover:bg-slate-100 focus:bg-slate-50 focus:ring focus:ring-slate-400 focus:ring-inset focus:ring-offset-1 text-slate-900 text-lg transition-all duration-75"
+            className="w-full rounded p-3 outline-none bg-slate-200 shadow-sm hover:bg-slate-100 focus:bg-slate-50 focus:ring focus:ring-slate-400 focus:ring-inset focus:ring-offset-1 text-slate-900 text-lg transition-all duration-75"
             disabled={isSubmitting}
             required
           />
@@ -71,7 +71,7 @@ export default function Form({ type }: FormProps) {
             type="submit"
             value={'SUBMIT'}
             disabled={isSubmitting}
-            className="w-full text-lg bg-slate-800 text-slate-300 tracking-wider py-3 rounded cursor-pointer hover:bg-slate-900 hover:scale-105 duration-100 transition-all"
+            className="w-full text-lg bg-slate-800 text-slate-300 tracking-wider py-3 rounded cursor-pointer shadow hover:shadow-lg hover:bg-slate-900 hover:scale-105 duration-100 transition-all"
           />
         </div>
       </form>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import Link from "next/link";
+
+export default function LandingPage() {
   return (
-    <div>home</div>
+    <div className="min-h-screen bg-slate-900 space-y-14">
+      <div className="px-4 sm:px-10 py-3 text-slate-400 text-lg">
+        <Link href={'/'}>ProMocha</Link>
+      </div>
+
+      <div className="space-y-10">
+        <h1 className="text-5xl sm:text-7xl text-center text-slate-200 px-4 sm:px-16">Run promotions <br /> like a <i className="font-bold">pro</i>.</h1>
+        <h2 className="text-xl sm:text-3xl text-center text-slate-300 px-4 sm:px-16">Manage promocodes and analytics <br /> all in one place.</h2>
+
+        <div className="flex justify-center space-x-3 text-xl">
+          <Link href={'/sign-in'} className="px-4 py-2 rounded-lg border border-slate-50 bg-slate-300">Sign in</Link>
+          <Link href={'/sign-up'} className="px-4 py-2 rounded-lg border border-sky-500 bg-sky-600">Sign up</Link>
+        </div>
+      </div>
+    </div>
   );
 }
